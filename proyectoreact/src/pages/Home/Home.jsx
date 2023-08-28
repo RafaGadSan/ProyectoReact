04d8da63981d178d317b4b404b703393c00d5d45
Ferrigorn <fbabures@gmail.com>
2023-08-28 23:29:58 +0200

    Home

--- proyectoreact/src/pages/Home/Home.jsx
+++ proyectoreact/src/pages/Home/Home.jsx
@@ -1,8 +1,26 @@
-import React from "react";
-import Header from "../../components/Header/Header";
+import "./Home.css";
+import { Flex, Box, Square, Text, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <Flex justify="center" p="20">
+      <Square size="300px">
+        <Text fontSize="2xl" as="cite" noOfLines={1}>
+          Entra y disfruta de nuestras recetas, ingredientes y toda su
+          información
+        </Text>
+        <Link><Button leftIcon={<LoginIcon />} colorScheme='teal' variant='solid'>Login</Button></Link>
+      </Square>
+      <Square size="300px">
+        <Text fontSize="2xl" as="cite" noOfLines={1}>
+          ¿Aún no estás registrado? ¡A qué esperas!
+        </Text>
+        <Link><Button>Register</Button></Link>
+      </Square>
+    </Flex>
+  );
 };
 
 export default Home;
