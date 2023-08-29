dc9e9917e56c582bcb2b320e5983baad7b33a127
Ferrigorn <fbabures@gmail.com>
2023-08-29 11:53:52 +0200

    Home

--- proyectoreact/src/pages/Home/Home.jsx
+++ proyectoreact/src/pages/Home/Home.jsx
@@ -5,19 +5,19 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <Flex justify="center" p="20">
+    <Flex justify="center" p="5" direction="column" align="center">
       <Square size="300px">
-        <Text fontSize="2xl" as="cite" noOfLines={1}>
+        <Text fontSize="2xl" as="cite" >
           Entra y disfruta de nuestras recetas, ingredientes y toda su
           información
         </Text>
-        <Link><Button leftIcon={<LoginIcon />} colorScheme='teal' variant='solid'>Login</Button></Link>
+        <Link to="/login"><Button  colorScheme='teal' variant='solid' p="5px">Login</Button></Link>
       </Square>
       <Square size="300px">
-        <Text fontSize="2xl" as="cite" noOfLines={1}>
+        <Text fontSize="2xl" as="cite" >
           ¿Aún no estás registrado? ¡A qué esperas!
         </Text>
-        <Link><Button>Register</Button></Link>
+        <Link to="/register"><Button>Register</Button></Link>
       </Square>
     </Flex>
   );
