edab7f8b6b39ad6842d75d7d99e02687d6e6fa0d
RafaGadSan <rafael.gadea.santos@gmail.com>
2023-09-03 21:14:20 +0200

    slint y prettier

--- proyectoreact/src/components/Header/Header.jsx
+++ proyectoreact/src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
-import { Box } from "@chakra-ui/react";
-import Navbar from "../NavBar/NavBar";
-import React from "react";
-import "./Header.css";
+import { Box } from "@chakra-ui/react"
+import Navbar from "../NavBar/NavBar"
+import React from "react"
+import "./Header.css"
 
 const Header = () => {
   return (
     <Box as="header">
       <Navbar></Navbar>
     </Box>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
